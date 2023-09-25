3022645b4b896a650fc5895eb8467026d7552860
AndreD4 <andremai629@gmail.com>
2023-09-25 17:20:14 +0900

    Built challenge
    print to command ask what favourite number from 1-100, type in answer then print response with comment and the input of favourite number

---
+++ Challenge/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int favourite_number;
+    std::cout << "Whats your favourite number between 1 - 100:";
+    std::cin >> favourite_number;
+    std::cout << "Wow thats my favourire number tooo!" <<std::endl;
+    std::cout << "No really," << favourite_number << " is my favourite number!" <<std::endl;
+    return 0;
+}
